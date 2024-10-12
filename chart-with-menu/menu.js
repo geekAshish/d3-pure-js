6423eac3afe9687c7a5f90cfcb1a3b10f9819f4e
geekAshish <ashishkushwaha472004@gmail.com>
2024-10-12 19:45:16 +0530

    menus on chart

---
+++ chart-with-menu/menu.js
@@ -0,0 +1,14 @@
+
+export const menu = () => {
+
+  const my = (selection) => {
+    selection.text('Ashish');
+  };
+
+  // 15:44
+  // my.width = function (_) {
+  //   return arguments.length ? ((width = +_), my) : width;
+  // };
+
+  return my;
+};
---
+++ chart-with-menu/scatter-plot-reusable-chart.js
@@ -0,0 +1,67 @@
+import { csv, select } from "d3";
+import { scatterPlot } from "./reusable-charts";
+import { menu } from "./menu";
+
+const parseRow = (d) => {
+  d.sepal_length = +d.sepal_length;
+  d.sepal_width = +d.sepal_width;
+  d.petal_length = +d.petal_length;
+  d.petal_width = +d.petal_width;
+  return d;
+};
+
+const svg = select("div")
+  .append("svg")
+  .attr("width", innerWidth)
+  .attr("height", innerHeight);
+
+const menuContainer = document.getElementById('svg-container');
+
+// data : https://github.com/curran/data
+const main = async () => {
+  const data = await csv(
+    "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv",
+    parseRow
+  );
+
+  // https://d3js.org/d3-scale
+  // const x = scaleLinear().domain([min(data, xValue), max(data, yValue)]);
+
+  const plot = scatterPlot()
+  .width(innerWidth)
+  .height(innerHeight)
+  .data(data)
+  .xValue((d) => d.petal_length)
+  .yValue((d) => d.sepal_length)
+  .margin({
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 50,
+  })
+  .radius(5)
+
+  svg.call(plot);
+
+  const columns = [
+    'petal_width',
+    'sepal_width',
+    'petal_length',
+    'sepal_length',
+  ];
+
+  menuContainer.call(menu())
+
+  // let i = 0;
+  // setInterval(() => { // instead of setInterval you should use requestAnimationFrame
+  //   plot.xValue((d) => d[columns[i % columns.length]]); // i%columns.length will give value between array
+  //   svg.call(plot);
+  //   i++;
+  // }, 3000);
+};
+main();
+
+/*
+domain
+range
+*/
